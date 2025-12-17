23c4d4b474e37e121839e3a8559f0fe3132b1c3b
PrahladKumar <prahladchoudhary7980@gmail.com>
2025-12-17 06:06:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function insert_Row() {
-    //Write your code here
-  
-  
+function insertRow() {
+    const tableBody = document.getElementById('sampleTable');
+
+    if (!tableBody) {
+        return;
+    }
+
+    const newRow = tableBody.insertRow(0);
+
+    const cell1 = newRow.insertCell(0);
+    
+    const cell2 = newRow.insertCell(1);
+
+    cell1.textContent = "New Cell";
+    cell2.textContent = "New Row";
 }
